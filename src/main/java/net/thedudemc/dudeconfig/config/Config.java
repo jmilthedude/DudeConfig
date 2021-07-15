02d88353d4ea7dbc8413b6b4227ba0374f054ebf
jmilthedude <30945719+jmilthedude@users.noreply.github.com>
2021-07-15 11:22:53 -0500

    get option public

--- src/main/java/net/thedudemc/dudeconfig/config/Config.java
+++ src/main/java/net/thedudemc/dudeconfig/config/Config.java
@@ -182,7 +182,7 @@ public abstract class Config {
         this.markDirty();
     }
 
-    private Option<?> getOption(String name) {
+    public Option<?> getOption(String name) {
         if (options.get(name) == null) throw new InvalidOptionException(name);
 
         return options.get(name);
